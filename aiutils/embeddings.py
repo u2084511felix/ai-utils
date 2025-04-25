412d75abc6a015f440c9fdcf2b94b7d9d65d5c52
u2084511felix <79988595+u2084511felix@users.noreply.github.com>
2025-04-25 20:39:38 +0100

    made embeddings async

--- aiutils/embeddings.py
+++ aiutils/embeddings.py
@@ -3,6 +3,6 @@ from openai import OpenAI
 from aiutils import client, EmbeddingModels
 
 
-def get_embedding(text, model=EmbeddingModels.large):
+async def get_embedding(text, model=EmbeddingModels.large):
     response = client.embeddings.create(input=text, model=model)
     return response.data[0].embedding
