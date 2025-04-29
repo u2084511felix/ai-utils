dbdb0167842326afd3c93e9c533045318319b51e
u2084511felix <79988595+u2084511felix@users.noreply.github.com>
2025-04-29 19:03:23 +0100

    added conditional to embedding model for default dimension sizes

--- aiutils/embeddings.py
+++ aiutils/embeddings.py
@@ -4,6 +4,10 @@ from aiutils import client, EmbeddingModels
 
 
 async def get_embedding(text, model=EmbeddingModels.large, dimensions=3072, encoding_format="float"):
+    if model == EmbeddingModels.small:
+        if dimensions > 1536:
+            dimensions = 1536
+
     response = client.embeddings.create(
         input=text,
         model=model,
