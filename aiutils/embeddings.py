dae42c588933f1ba7636fdf06baa8eff0694d64e
u2084511felix <79988595+u2084511felix@users.noreply.github.com>
2025-04-29 01:53:44 +0100

    created embedding params for dimensions

--- aiutils/embeddings.py
+++ aiutils/embeddings.py
@@ -3,6 +3,11 @@ from openai import OpenAI
 from aiutils import client, EmbeddingModels
 
 
-async def get_embedding(text, model=EmbeddingModels.large):
-    response = client.embeddings.create(input=text, model=model)
+async def get_embedding(text, model=EmbeddingModels.large, dimensions=3072, encoding_format="float"):
+    response = client.embeddings.create(
+        input=text,
+        model=model,
+        encoding_format=encoding_format,
+        dimensions=dimensions
+    )
     return response.data[0].embedding
